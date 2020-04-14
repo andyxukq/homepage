6e1465a7fd622ce3fc279ed4960d519d4f017247
Andy Xu <a@andyxu.xyz>
2020-04-14 11:50:47 -0700

    url tweak for remote html

---
+++ docs/precache-manifest.560903db541d42fd585bb314974c34e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9500eae075168c8661f360e04fc8a3f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc96a0098dafd57ddd76",
+    "url": "./static/css/2.dd1866a9.chunk.css"
+  },
+  {
+    "revision": "bf882db55a032bcf23f4",
+    "url": "./static/css/main.0480dc06.chunk.css"
+  },
+  {
+    "revision": "dc96a0098dafd57ddd76",
+    "url": "./static/js/2.4c5d9552.chunk.js"
+  },
+  {
+    "revision": "0adf7f1b9434a120a5bce5b5824fa305",
+    "url": "./static/js/2.4c5d9552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf882db55a032bcf23f4",
+    "url": "./static/js/main.8a9ff5bf.chunk.js"
+  },
+  {
+    "revision": "3be4fba92fd10a3afd88",
+    "url": "./static/js/runtime-main.b094c791.js"
+  },
+  {
+    "revision": "42979a059e628bc4d826ea921faf8869",
+    "url": "./static/media/Gibson-Book.42979a05.otf"
+  },
+  {
+    "revision": "4f6a8c74022c23a2899a3e595d6a04f0",
+    "url": "./static/media/Gibson-Regular.4f6a8c74.otf"
+  },
+  {
+    "revision": "5436eb62561957fd9d8522530aa72113",
+    "url": "./static/media/Gibson-SemiBold.5436eb62.otf"
+  },
+  {
+    "revision": "e9ba56ea7495ff4b1d57a99d69095321",
+    "url": "./static/media/portrait.e9ba56ea.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://page.andyxu.xyz/precache-manifest.28b619cb9595c8703dee74243e04ff80.js"
+  "./precache-manifest.560903db541d42fd585bb314974c34e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://page.andyxu.xyz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
