73fd5ffd10a5e8202e0c3b29a45530ecbcfbc6bf
Andy Xu <andyxukq@gmail.com>
2023-09-29 08:45:14 +0800

    update Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,14 +7,14 @@ class Bio extends React.Component {
 		return (
 			<div className="Bio container">
 				<div className="columns">
-					<div className="column">
-						<div className="short-bio" dangerouslySetInnerHTML={{"__html":this.props.short_bio}}></div>
-					</div>
 					<div className="column is-narrow">
 						<div className="portrait">
 							<img src={portrait.path} alt="portrait"/>
 							<p className="caption">{portrait.caption}</p>
 						</div>
+					</div>
+					<div className="column">
+						<div className="short-bio" dangerouslySetInnerHTML={{"__html":this.props.short_bio}}></div>
 					</div>
 				</div>
 			</div>
