e53f2ba421ffb3a220ff87de325338330c56fac6
Andy Xu <andyxukq@gmail.com>
2025-05-10 17:51:40 -0700

    update papers

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     //append utm_source to all links
     document.querySelectorAll('a').forEach(function(link) {
-        if (link.href.indexOf('http') === 0 && link.href.indexOf('kqxu.com') < 0 && link.href.indexOf('127.0.0.1') < 0 && link.href.indexOf('/') > 0) {
+        if (link.href.indexOf('kqxu.com') < 0 && link.href.indexOf('127.0.0.1') < 0 && link.href.indexOf('/') > 0 || link.href.indexOf('/papers') > 0) {
             link.setAttribute('target', '_blank');
         }
     });
